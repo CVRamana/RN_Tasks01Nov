36cd49db9507ca3ac64e62584c12b19b30d6bef1
CVRamana <ramanverma693@gmail.com>
2019-11-09 13:36:25 +0530

    responsive web

---
+++ RNDimention/App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+ 
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.responsiveBox}>
+          <Text style={styles.text}>This box is always of 84.5% width and 17% height.</Text>
+          <Text style={styles.text}>Test it by running this example repo in phones/
+            emulators with screens of various dimensions and pixel per inch (ppi).</Text>
+        </View>
+
+        <View>
+          
+          </View>
+
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  responsiveBox: {
+    width: wp('95%'),
+    height: hp('17%'),
+    borderWidth: 2,
+    borderColor: 'orange',
+    flexDirection: 'column',
+    justifyContent: 'space-around' 
+  },
+  text: {
+    color: 'white',
+
+  }
+});

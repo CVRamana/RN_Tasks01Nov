6697d1cf62c7643747f7b8483537010b856cb886
CVRamana <ramanverma693@gmail.com>
2019-11-13 22:12:04 +0530

    text color changes on click

--- ResponsiveUI/ReusableComponent/FlatListTodoComp.js
+++ ResponsiveUI/ReusableComponent/FlatListTodoComp.js
@@ -5,27 +5,28 @@ import { View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-n
 class FlatListTodoComp extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            ischecked:false
+        this.state = {
+            ischecked: false
         }
     }
-   
+
 
     render() {
         return (
             <View style={styles.supreme}>
                 <View style={styles.parent}>
-                    <Text>{this.props.name}</Text>
+                    <Text style={this.state.ischecked ? styles.txt1 :styles.txt2}>{this.props.name}</Text>
                     <Text>{this.props.id}</Text>
-                   {/* // <Text> {JSON.stringify(this.props.object11)}</Text> */}
+                    {/* // <Text> {JSON.stringify(this.props.object11)}</Text> */}
                 </View>
                 <View style={styles.bttnContainer}>
                     <TouchableOpacity
-                    onPress={()=>{
-                        console.warn(this.state.ischecked)
-                        this.setState({
-                        ischecked:!this.state.ischecked
-                    })}} 
+                        onPress={() => {
+                            console.warn(this.state.ischecked)
+                            this.setState({
+                                ischecked: !this.state.ischecked
+                            })
+                        }}
                     >
                         <Image
                             style={styles.checkImg}
@@ -34,7 +35,7 @@ class FlatListTodoComp extends Component {
                     </TouchableOpacity>
                     <Button
                         title="DELETE"
-                        onPress={()=>this.props.deletTask(this.props.id)}
+                        onPress={() => this.props.deletTask(this.props.id)}
                     />
                 </View>
             </View>
@@ -47,29 +48,45 @@ const styles = StyleSheet.create({
     parent: {
         width: 280,
         height: 100,
-        justifyContent:"center",
+        justifyContent: "center",
         //alignItems: 'center',
-       // backgroundColor:"yellow"
+        // backgroundColor:"yellow"
     },
     supreme: {
-        
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: "pink",
         flexDirection: 'row',
         margin: 10,
-        borderRadius: 20
+        borderRadius: 20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.53,
+        shadowRadius: 13.97,
+
+        elevation: 21,
     },
     bttnContainer: {
-       // backgroundColor: "lightgrey",
-        justifyContent:"center",
-        alignItems:"center"
+        // backgroundColor: "lightgrey",
+        justifyContent: "center",
+        alignItems: "center"
     },
     checkImg: {
         height: 40,
-        width: 40, 
+        width: 40,
         //backgroundColor: "red",
-        borderRadius:20
+        borderRadius: 20
+    },
+    txt1:{
+        fontSize:20,
+        color:"#c3565f"
+    },
+    txt2:{
+        fontSize:20,
+        color:"green"
     }
 
 })
